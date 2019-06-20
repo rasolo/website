14fc9f8e88511329b9fb77df541fdeda060c4301
Rasmus <rasmus.olofsson@creuna.se>
2019-06-20 19:00:46 +0200

    Add test project

---
+++ src/Rasolo.Tests/StartPageControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Moq;
+using NUnit.Framework;
+using Umbraco.Core.Composing;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace Rasolo.Tests {
+    public class StartPageControllerTests
+    {
+        private Mock<IPublishedContent> content;
+
+        [SetUp]
+        public void SetUp() {
+            Current.Factory = Mock.Of<IFactory>();
+            this.content = new Mock<IPublishedContent>();
+        }
+
+    }
+}
