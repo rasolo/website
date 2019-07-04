806c8fd31160c3f1d9fbf9ef0b173750d1a67ce5
Rasmus <rasmus.olofsson@creuna.se>
2019-07-04 14:50:06 +0200

    Unit test Start page returns with title if it is set

--- src/Rasolo.Core/Features/StartPage/StartPage.cs
+++ src/Rasolo.Core/Features/StartPage/StartPage.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Rasolo.Core.Features.StartPage
+﻿namespace Rasolo.Core.Features.StartPage
 {
 	public class StartPage
 	{
 		public virtual string Name { get; set; }
+		public virtual string Title { get; set; }
 	}
 }
--- src/Rasolo.Tests/StartPageControllerTests.cs
+++ src/Rasolo.Tests/StartPageControllerTests.cs
@@ -1,9 +1,13 @@
-﻿using Moq;
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Moq;
 using NUnit.Framework;
 using Rasolo.Core.Features.Shared.Mappings;
 using Rasolo.Core.Features.StartPage;
 using System.Web.Mvc;
 using Umbraco.Core.Composing;
+using Umbraco.Core.Models;
 using Umbraco.Core.Models.PublishedContent;
 using Umbraco.Web.Models;
 
@@ -40,6 +44,32 @@ namespace Rasolo.Tests
 			var viewModel = (StartPage)((ViewResult)this._sut.Index(publishedContentMock)).Model;
 
 			Assert.AreEqual(expected, viewModel.Name);
+		}
+
+		[Test]
+		[TestCase("Page title", "Page title")]
+		[TestCase("Another Page title", "Another Page title")]
+		public void GivenPageHasTitle_WhenIndexAction_ThenReturnViewModelWithPageTitle(string title, string expected)
+		{
+			var pageName = "startPage";
+			var propertyAlias = "title";
+
+			var property = new Mock<IPublishedProperty>();
+			var content = new Mock<IPublishedContent>();
+			property.Setup(x => x.Alias).Returns(propertyAlias);
+			property.Setup(x => x.HasValue(It.IsAny<string>(),It.IsAny<string>())).Returns(title != null);
+			property.Setup(x => x.GetValue(It.IsAny<string>(), It.IsAny<string>())).Returns(title);
+			content.Setup(x => x.ContentType).Returns(new PublishedContentType(1234, pageName, PublishedItemType.Content,
+			Enumerable.Empty<string>(), Enumerable.Empty<PublishedPropertyType>(), ContentVariation.Nothing));
+			content.Setup(c => c.GetProperty(It.Is<string>(x => x == propertyAlias))).Returns(property.Object);
+			content.Setup(x => x.Name).Returns(pageName);
+			content.Setup(x => x.Id).Returns(99);
+
+
+			var publishedContentMock = new ContentModel(content.Object);
+			var viewModel = (StartPage)((ViewResult)this._sut.Index(publishedContentMock)).Model;
+
+			Assert.AreEqual(expected, viewModel.Title);
 		}
 	}
 }
