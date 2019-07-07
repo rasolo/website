ec55152bbe274d49ae7621b56a8c8b606de9b23f
Rasmus <rasmus.olofsson@creuna.se>
2019-07-07 18:25:33 +0200

    Create BlogPostPage type

---
+++ src/Rasolo.Core/Features/BlogPostPage/BlogPostPage.cs
@@ -0,0 +1,8 @@
+﻿using Rasolo.Core.Features.Shared.UI;
+
+namespace Rasolo.Core.Features.BlogPostPage
+{
+	public class BlogPostPage : BaseContentPage
+	{
+	}
+}
---
+++ src/Rasolo.Tests/Unit/Features/BlogPostPage/BlogPostPageControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using Rasolo.Core.Features.BlogPostPage;
+using Rasolo.Core.Features.Shared.Mappings;
+using Rasolo.Tests.Unit.Shared;
+
+namespace Rasolo.Tests.Unit.Features.BlogPostPage
+{
+	public class BlogPostPageControllerTests : BaseContentPageControllerTests<Core.Features.BlogPostPage.BlogPostPage>
+	{
+		public override void SetUp()
+		{
+			base.SetUp();
+			var umbracoMapper = new UmbracoMapperComposer().SetupMapper();
+			this._sut = new BlogPostPageController(umbracoMapper);
+		}
+	}
+}
