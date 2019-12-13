3df9d6d4b05313e7bf348bfa330d473d5309522b
Rasmus Olofsson <rasmus.olofsson@creuna.se>
2019-12-13 21:11:00 +0100

    Add Umbraco helper

---
+++ src/Rasolo.Core/Features/Shared/Abstractions/UmbracoHelper/IUmbracoHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace Rasolo.Core.Features.Shared.Abstractions.UmbracoHelper
+{
+	public interface IUmbracoHelper
+	{
+		IEnumerable<IPublishedContent> ContentAtRoot();
+	}
+}
---
+++ src/Rasolo.Core/Features/Shared/Abstractions/UmbracoHelper/UmbracoHelperAdapter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace Rasolo.Core.Features.Shared.Abstractions.UmbracoHelper
+{
+	public class UmbracoHelperAdapter : IUmbracoHelper
+	{
+		private readonly Umbraco.Web.UmbracoHelper _umbracoHelper;
+
+		public UmbracoHelperAdapter(Umbraco.Web.UmbracoHelper umbracoHelper)
+		{
+			this._umbracoHelper = umbracoHelper;
+		}
+
+		public IEnumerable<IPublishedContent> ContentAtRoot()
+		{
+			return _umbracoHelper.ContentAtRoot();
+		}
+	}
+}
