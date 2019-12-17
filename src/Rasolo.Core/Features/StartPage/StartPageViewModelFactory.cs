50c436753f7bbc23b40792efb93dfe3aa1fdbea2
Rasmus Olofsson <rasmus.olofsson@creuna.se>
2019-12-17 21:06:37 +0100

    Remove no longer relevant todo

--- src/Rasolo.Core/Features/StartPage/StartPageViewModelFactory.cs
+++ src/Rasolo.Core/Features/StartPage/StartPageViewModelFactory.cs
@@ -2,27 +2,24 @@
 using System.Linq;
 using Rasolo.Core.Features.Shared.Compositions;
 using Rasolo.Core.Features.Shared.Constants;
-using Rasolo.Core.Features.Shared.Constants.MediaCropAliases.BlogPostPage;
-using Rasolo.Core.Features.Shared.Constants.PropertyTypeAlias;
 using Rasolo.Core.Features.Shared.Services;
-using Umbraco.Core.Models.PublishedContent;
-using Umbraco.Web;
 using Zone.UmbracoMapper.V8;
 
 namespace Rasolo.Core.Features.StartPage
 {
 	public class StartPageViewModelFactory : BaseContentPageViewModelFactory<StartPage>, IStartPageViewModelFactory
 	{
+		private readonly IBlogPostService _blogPostService;
 		private readonly IUmbracoMapper _umbracoMapper;
 		private readonly IUmbracoService _umbracoService;
-		private readonly IBlogPostService _blogPostService;
 
 
-		public StartPageViewModelFactory(IUmbracoMapper mapper, IUmbracoService umbracoService, IBlogPostService blogPostService)
+		public StartPageViewModelFactory(IUmbracoMapper mapper, IUmbracoService umbracoService,
+			IBlogPostService blogPostService)
 		{
-			this._umbracoMapper = mapper;
-			this._umbracoService = umbracoService;
-			this._blogPostService = blogPostService;
+			_umbracoMapper = mapper;
+			_umbracoService = umbracoService;
+			_blogPostService = blogPostService;
 		}
 
 		public override StartPage CreateModel(StartPage viewModel)
@@ -30,24 +27,24 @@ namespace Rasolo.Core.Features.StartPage
 			viewModel = base.CreateModel(viewModel);
 
 			SetBlogPagesOnViewModel(viewModel);
-			viewModel.BlogPostPages = this._blogPostService
-                .GetMappedBlogPosts(_umbracoService
-                .GetAllPagesByDocumentTypeAtRootLevel(DocumentTypeAlias.BlogPostPage)).ToList();
+			viewModel.BlogPostPages = _blogPostService
+				.GetMappedBlogPosts(_umbracoService
+					.GetAllPagesByDocumentTypeAtRootLevel(DocumentTypeAlias.BlogPostPage)).ToList();
 
 			return viewModel;
 		}
 
-		//TODO: Refractor methods below, make dry
 		private void SetBlogPagesOnViewModel(StartPage viewModel)
 		{
-			var blogPagesAsPublishedContent = _umbracoService.GetAllPagesByDocumentTypeAtRootLevel(DocumentTypeAlias.BlogPage).ToList();
+			var blogPagesAsPublishedContent =
+				_umbracoService.GetAllPagesByDocumentTypeAtRootLevel(DocumentTypeAlias.BlogPage).ToList();
 
 			viewModel.BlogPages = new List<BlogPage.BlogPage>();
 
 			foreach (var blogPagePublishedContent in blogPagesAsPublishedContent)
 			{
 				var blogPage = new BlogPage.BlogPage();
-				this._umbracoMapper.Map(blogPagePublishedContent, blogPage);
+				_umbracoMapper.Map(blogPagePublishedContent, blogPage);
 
 				viewModel.BlogPages.Add(blogPage);
 			}
