c941df67a00159d783a4e561fcf80dff5e31d87b
Rasmus Olofsson <rasmus.olofsson@knowit.se>
2022-02-17 16:38:17 +0100

    Display individual blogpostpages fix

--- src/Rasolo.Web/Features/BlogPostPage/BlogPostPageController.cs
+++ src/Rasolo.Web/Features/BlogPostPage/BlogPostPageController.cs
@@ -1,7 +1,9 @@
 ﻿using Anaximapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Microsoft.Extensions.Logging;
 using Rasolo.Web.Features.Shared.Compositions;
+using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Web;
 using Umbraco.Cms.Web.Common.Controllers;
 
@@ -11,6 +13,18 @@ namespace Rasolo.Web.Features.BlogPostPage
 	{
 		public BlogPostPageController(IPublishedContentMapper anaxiMapper, IBlogPostPageViewModelFactory viewModelFactory, ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) : base(anaxiMapper, viewModelFactory, logger, compositeViewEngine, umbracoContextAccessor)
 		{
+			ViewModelFactory = viewModelFactory;
+		}
+
+		public IBlogPostPageViewModelFactory ViewModelFactory { get; }
+
+		public IActionResult BlogPostPage(ContentModel contentModel)
+		{
+			var mappedModel = this.MapModel(contentModel.Content);
+			var viewModel = this.ViewModelFactory.CreateModel(mappedModel, contentModel);
+
+			return View(viewModel);
+
 		}
 	}
 }
