0d7927cbf00444a2b5c5f5a60007cae5c2bb4170
Rasmus Olofsson <rasmus.olofsson@knowit.se>
2022-04-26 15:18:41 +0200

    Add basic test project

---
+++ src/Rasolo.Tests/Unit/Features/Start/StartPageControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using Anaximapper;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using Rasolo.Web.Features.Shared.Compositions;
+using Rasolo.Web.Features.StartPage;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+using UmbracoNineDemoSite.Tests.Extensions;
+
+namespace Rasolo.Tests.Unit.Features.Start
+{
+    [TestFixture]
+    public class StartPageControllerTests
+    {
+        private StartPageController controller;
+        public Mock<IStartPageViewModelFactory> viewModelFactory;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.viewModelFactory = new Mock<IStartPageViewModelFactory>();
+            this.controller = new StartPageController(Mock.Of<IPublishedContentMapper>(), viewModelFactory.Object, Mock.Of<ILogger<RenderController>>(), Mock.Of<ICompositeViewEngine>(), Mock.Of<IUmbracoContextAccessor>());
+        }
+
+        [Test]
+        public void Given_Controller_When_IndexAction_Then_ViewModelFactoryIsCalled()
+        {
+            var publishedContent = new Mock<IPublishedContent>();
+            publishedContent.SetupPropertyValue("whatever", "whatever");
+            var contentModel = new ContentModel(publishedContent.Object);
+
+            var viewModel = new StartPage();
+            viewModelFactory.Setup(x => x.CreateModel(It.IsAny<StartPage>(), null)).Returns(viewModel);
+            this.controller.StartPage(contentModel);
+
+            viewModelFactory.Verify(x => x.CreateModel(It.IsAny<StartPage>(), contentModel), Times.Exactly(1));
+        }
+    }
+}
