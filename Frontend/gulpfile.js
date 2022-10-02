02d4a72e805525ddf10df57f156d91536ed1a9b1
Rasmus Olofsson <rasmus.olofsson@knowit.se>
2022-10-02 22:00:21 +0200

    Make gulp file work again

--- Frontend/gulpfile.js
+++ Frontend/gulpfile.js
@@ -2,68 +2,97 @@
 // Importing specific gulp API functions lets us write them below as series() instead of gulp.series()
 const { src, dest, watch, series, parallel } = require('gulp');
 // Importing all the Gulp-related packages we want to use
-const browserify = require("browserify");
-const sourcemaps = require('gulp-sourcemaps');
-const sass = require('gulp-sass');
-const uglify = require('gulp-uglify');
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const terser = require('gulp-terser');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
-var replace = require('gulp-replace');
-const source = require("vinyl-source-stream");
-const gulp = require('gulp');
-
+const replace = require('gulp-replace');
+const browsersync = require('browser-sync').create();
 
 // File paths
-const files = { 
-    scssPath: 'src/scss/*.scss',
-    jsPath: 'src/**/*.js'
+const files = {
+	scssPath: 'src/scss/*.scss',
+    jsPath: 'src/commponents/*.js'
+};
+
+// Sass task: compiles the style.scss file into style.css
+function scssTask() {
+	return src(files.scssPath, { sourcemaps: true }) // set source and turn on sourcemaps
+		.pipe(sass()) // compile SCSS to CSS
+		.pipe(postcss([autoprefixer(), cssnano()])) // PostCSS plugins
+		.pipe(dest('../src/Rasolo.Web/wwwroot/assets/css'))
+}
+
+// JS task: concatenates and uglifies JS files to script.js
+function jsTask() {
+	return src(
+		[
+			files.jsPath,
+			//,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
+		],
+		{ sourcemaps: true }
+	)
+		.pipe(concat('bundle.js'))
+		.pipe(terser())
+		.pipe(dest('../src/Rasolo.Web/wwwroot/assets/js', { sourcemaps: '.' }));
 }
 
 
-async function scssTask(){   
- gulp.task('scssTask', async function() {
-    return src(files.scssPath)
-    .pipe(sourcemaps.init()) // initialize sourcemaps first
-    .pipe(sass()) // compile SCSS to CSS
-    .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
-    .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
-    .pipe(dest('../src/Rasolo.Web/wwwroot/assets/css'))
-  });
+// Browsersync to spin up a local server
+function browserSyncServe(cb) {
+	// initializes browsersync server
+	browsersync.init({
+		server: {
+			baseDir: '.',
+		},
+		notify: {
+			styles: {
+				top: 'auto',
+				bottom: '0',
+			},
+		},
+	});
+	cb();
+}
+function browserSyncReload(cb) {
+	// reloads browsersync server
+	browsersync.reload();
+	cb();
 }
 
-async function jsTask() {
-gulp.task('jsTask', async function() {
-    return  (browserify({
-        entries: ["./src/components/app.js"]
-    })
-               // Bundle it all up!
-               .bundle()
-               // Source the bundle
-               .pipe(source("bundle.js"))
-               // Then write the resulting files to a folder
-               .pipe(dest("../src/Rasolo.Web/wwwroot/assets/js"))
-    );
-  });
-}
 // Watch task: watch SCSS and JS files for changes
 // If any change, run scss and js tasks simultaneously
-function watchTask(){
-    watch([files.scssPath, files.jsPath], {interval: 1000, usePolling: true}, 
-        series(
-            parallel(scssTask, jsTask),
-        )
-    );    
+function watchTask() {
+	watch(
+		[files.scssPath, files.jsPath],
+		{ interval: 1000, usePolling: true }, //Makes docker work
+		series(parallel(scssTask, jsTask))
+	);
+}
+
+// Browsersync Watch task
+// Watch HTML file for change and reload browsersync server
+// watch SCSS and JS files for changes, run scss and js tasks simultaneously and update browsersync
+function bsWatchTask() {
+	watch('index.html', browserSyncReload);
+	watch(
+		[files.scssPath, files.jsPath],
+		{ interval: 1000, usePolling: true }, //Makes docker work
+		series(parallel(scssTask, jsTask), browserSyncReload)
+	);
 }
 
 // Export the default Gulp task so it can be run
 // Runs the scss and js tasks simultaneously
-// then runs cacheBust, then watch task
-exports.default = series(
-    parallel(scssTask, jsTask), 
-    watchTask
-);
 
-exports.dist = series(
-    parallel(scssTask, jsTask), 
+exports.default = series(parallel(scssTask, jsTask));
+
+// Runs all of the above but also spins up a local Browsersync server
+// Run by typing in "gulp bs" on the command line
+exports.bs = series(
+	parallel(scssTask, jsTask),
+	browserSyncServe,
+	bsWatchTask
 );
