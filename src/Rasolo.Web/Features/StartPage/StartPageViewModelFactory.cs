ca4e7559121d36acade517348eb093a91bd07803
Rasmus Olofsson <rasmus.olofsson@knowit.se>
2023-04-14 19:18:28 +0200

    BE: Rename stuff

--- src/Rasolo.Web/Features/StartPage/StartPageViewModelFactory.cs
+++ src/Rasolo.Web/Features/StartPage/StartPageViewModelFactory.cs
@@ -5,7 +5,6 @@ using Rasolo.Web.Features.Shared.Compositions;
 using Rasolo.Web.Features.Shared.Services;
 using Rasolo.Services.Abstractions.UmbracoHelper;
 using Rasolo.Services.Constants;
-using Umbraco.Cms.Core.Mapping;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Extensions;
 using Rasolo.Web.Features.Shared.Models;
@@ -15,14 +14,14 @@ namespace Rasolo.Web.Features.StartPage
 	public class StartPageViewModelFactory : BaseContentPageViewModelFactory<StartPage>, IStartPageViewModelFactory
 	{
 		private readonly IBlogPostService _blogPostService;
-		private readonly IPublishedContentMapper anaxiMapper;
+		private readonly IPublishedContentMapper _anaxiMapper;
 		private readonly IUmbracoService _umbracoService;
 
 
 		public StartPageViewModelFactory(IPublishedContentMapper anaxiMapper, IUmbracoService umbracoService,
 			IBlogPostService blogPostService, IUmbracoHelper umbracoHelper) : base(anaxiMapper, umbracoHelper)
 		{
-			this.anaxiMapper = anaxiMapper;
+			_anaxiMapper = anaxiMapper;
 			_umbracoService = umbracoService;
 			_blogPostService = blogPostService;
 		}
@@ -30,7 +29,7 @@ namespace Rasolo.Web.Features.StartPage
 		public override void SetViewModelProperties(StartPage viewModel, ContentModel contentModel)
 		{
 			base.SetViewModelProperties(viewModel, contentModel);
-			SetBlogPagesOnViewModel(viewModel);
+			SetBlogPages(viewModel);
 			viewModel.BlogPostPages = _blogPostService
 				.GetMappedBlogPosts(_umbracoService
 					.GetAllPagesByDocumentTypeAtRootLevel(DocumentTypeAlias.BlogPostPage)).Take(4).ToList();
@@ -38,7 +37,6 @@ namespace Rasolo.Web.Features.StartPage
 			SetButtons(viewModel);
 
 			viewModel.Title = viewModel.Title.StripHtml().Replace("\n", "<br />");
-
 		}
 
 		private static void SetButtons(StartPage viewModel)
@@ -62,7 +60,7 @@ namespace Rasolo.Web.Features.StartPage
 			};
 		}
 
-		private void SetBlogPagesOnViewModel(StartPage viewModel)
+		private void SetBlogPages(StartPage viewModel)
 		{
 			var blogPagesAsPublishedContent =
 				_umbracoService.GetAllPagesByDocumentTypeAtRootLevel(DocumentTypeAlias.BlogPage).ToList();
@@ -72,7 +70,7 @@ namespace Rasolo.Web.Features.StartPage
 			foreach (var blogPagePublishedContent in blogPagesAsPublishedContent)
 			{
 				var blogPage = new BlogPage.BlogPage();
-				this.anaxiMapper.Map(blogPagePublishedContent, blogPage);
+				this._anaxiMapper.Map(blogPagePublishedContent, blogPage);
 
 				viewModel.BlogPages.Add(blogPage);
 			}
